e69c7acfbcf2a97eeb8afff4b7958fd6afd09d6b
Davis Ochieng <davisochieng81@gmail.com>
2026-03-06 02:25:36 +0300

    Used weights and inputs for hidden layer

--- main.cpp
+++ main.cpp
@@ -8,11 +8,11 @@ int main() {
 	
 	double hidden_layer[2];
 
-	for ( int i = 1; i <= 2; i++ )
+	for ( int i = 0; i < 2; i++ )
 	{
-		for ( int j = 1; j <= 2; j++ )
+		for ( int j = 0; j < 2; j++ )
 		{
-			hidden_layer[i] += i * j; 
+			hidden_layer[i] += x[i] * weights[j]; 
 		}
 
 		printf("%lf ", hidden_layer[i] += bias);
