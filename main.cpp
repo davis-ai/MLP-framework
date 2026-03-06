cc2f55ac85a4dcc9a4c562f2f99f5e726e5b791f
Davis Ochieng <davisochieng81@gmail.com>
2026-03-06 04:55:54 +0300

    Created a function to handle layer creation

--- main.cpp
+++ main.cpp
@@ -1,22 +1,33 @@
 #include <stdio.h>
 
-int main() {
-	
-	int x[] = { 1, 2 };
-	double weights[] = { 1.0, 1.0 };
+#define len(x) (sizeof(x) / sizeof(x[0]))
+
+double hidden_layer[2];
+
+void neural_network ( int inputs[], double weights[] ) {
+
+
 	double bias = 1.0;
-	
-	double hidden_layer[2];
 
 	for ( int i = 0; i < 2; i++ )
 	{
 		for ( int j = 0; j < 2; j++ )
 		{
-			hidden_layer[i] += x[i] * weights[j]; 
+			hidden_layer[i] += inputs[i] * weights[j]; 
 		}
 
 		printf("%lf ", hidden_layer[i] += bias);
 	}
+
+}
+
+int main() {
+	
+	int x[] = { 1, 2 };
+	double weights[] = { 1.0, 1.0 };
+	
+	
+	neural_network( x, weights );
 
 	printf("\n");
 	
