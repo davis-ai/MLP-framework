396cbfb01c492c7bc2c8d3f4a9b7bccb5200b3fb
Davis Ochieng <davisochieng81@gmail.com>
2026-07-20 00:42:48 +0300

    Created an external file for handling the function

--- main.c
+++ main.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "functions.h"
 
 #define len(x) (sizeof(x) / sizeof(x[0]))
-
-double* elem_mult ( const double[], double[], int );
-double dot ( const double[], double[], int );
-double* vec_mat_mult ( int, int, const double[*], double[*][*] );
-double* neural_network (  int, int, double[], double[*][*] ); 
-
-void print( int, double*, char* );
-void free_mem ( int, double* );
-
-void backProp ( int, int, double[], double[][*] );
-double* delta ( double[], double[] );
 
 
 int output_size = 3;
@@ -71,77 +61,4 @@ int main() {
 
 
 
-double* neural_network ( int size_input, int row_weight, double Input[], double Weight[row_weight][size_input] )
-{
-	double* hidden = vec_mat_mult ( size_input, row_weight, Input, Weight );
 
-	return hidden; 
-}
-
-
-double dot ( const double Inp[], double Wgt[], int size) {
-
-	double C = 0;
-
-	for ( int i = 0; i < size; i++ )
-       	    C += Inp[i] * Wgt[i];			
-	
-	return C;
-
-}
-
-double* vec_mat_mult ( int size_inp, int r_wgt, const double input[size_inp], double weight[r_wgt][size_inp] ) 
-{
-	double* result = calloc( r_wgt, sizeof(double) );
-	
-	for ( int i = 0; i < r_wgt; i++ )
-	    result[i] = dot( input, weight[i], size_inp );
-
-	return result; 
-
-}
-
-void backProp ( int a, int b, double A[], double B[][3] ) {
-
-	
-
-}
-
-double* delta ( double prediction[], double output[] ) {
-
-	double *pure_error = (double*) malloc(sizeof(double) * output_size );
-	
-	for ( int i = 0; i < output_size; i++ )
-		pure_error[i] = prediction[i] - output[i];
-	
-	return pure_error;
-
-
-}
-
-double* elem_mult ( const double A[], double B[], int prod_size ) {
-
-	double* prod = (double*)malloc(sizeof(double) * prod_size);
-	for ( int i = 0; i < prod_size; i++ ) prod[i] = A[i] * B[i];
-	return prod;
-}
-
-void print( int layer_size, double* layer, char* layer_type ) {
-
-	printf("%s : \n", layer_type);
-
-	for ( int i = 0; i < layer_size ; i++ ) 
-	    printf( "%lf ", layer[i] );
-
-
-	printf("\n");
-
-
-
-}
-
-void free_mem( int row_A, double* layer) {
-
-	free(layer);
-
-}
